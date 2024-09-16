e347f2d8b7ea81ffe32094ba31a7e74328dd9237
udaykiran6463 <udaykiran6463@gmail.com>
2024-09-16 17:36:24 +0530

    revise

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
-      <h1>chai aur react with vite | uday kiran pillalamarri</h1>
-    </div>
-  )
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>chai aur react with vite | uday kiran pillalamarri</h1>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
+
+
+
+import React from "react";
+import Main from "./Main.jsx";
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Main/>
+)
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App />
+// )
+
+
+function Main() {
+    return (
+        <div>
+            <h1>In the main file</h1>
+        </div>
+    )
+}
+
+export default Main;
+
+
