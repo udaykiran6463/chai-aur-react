39a7021807d32b95d4af17a7248527fddff5b556
udaykiran6463 <udaykiran6463@gmail.com>
2024-10-21 21:01:13 +0530

    react hook form starting

--- Blogverse/src/components/inputs/Input.jsx
+++ Blogverse/src/components/inputs/Input.jsx
@@ -13,10 +13,18 @@ const Input = forwardRef(function Input({
                 label && (
                     <label  className='inline-block mb-1 pl-1' htmlFor={id} >
                         {label}
-                        <input type={type} ref={ref} className= {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} id={id}  {...props} />
                     </label>
                     )
             }
+            <input 
+                type={type} 
+                ref={ref} 
+                className= {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} 
+                id={id}  
+                {...props} 
+                
+            />
+            
         </div>  
     )
 })
